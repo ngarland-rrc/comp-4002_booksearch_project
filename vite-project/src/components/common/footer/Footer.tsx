8bf2914fb3d307f1eb45af757ab9bf7d7b91055d
Nick Garland <ngarland@academic.rrc.ca>
2026-09-14 21:08:20 -0500

    feat: added a piblic folder to hold asset images and added to the footer

--- vite-project/src/components/common/footer/Footer.tsx
+++ vite-project/src/components/common/footer/Footer.tsx
@@ -2,7 +2,22 @@ import "./Footer.css"
 
 function Footer() {
     return(<footer>
-        Place holder &#169; The Book Search Team, {new Date().getFullYear()}
+        <div className= "social_row">
+            <a href=""><img src="/assets/social-icons/facebook.png" alt="Facebook" /></a>
+            <a href=""><img src="/assets/social-icons/youtube.png" alt="Youtube" /></a>
+            <a href=""><img src="/assets/social-icons/instagram.png" alt="Instagram" /></a>
+            <a href=""><img src="/assets/social-icons/twitter.png" alt="Twitter" /></a>
+        </div>
+        <div className = "ft-list-row">
+            <li><a href="">Contact us</a></li>
+            <li><a href="">Our Services</a></li>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Terms & Conditions</a></li>
+            <li><a href="">Career</a></li>
+        </div>
+        <div className = "copy-write">
+            Place holder &#169; The Book Search Team, {new Date().getFullYear()}
+        </div>
     </footer>);
 }
 
